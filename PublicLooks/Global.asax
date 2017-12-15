4b73d6e9ac56b1de26fd5b774d3385b2e5b08edf
MD Shohag Mia <md-shohag-mia@outlook.com>
2017-12-15 11:51:46 +0600

    Add project files.

---
+++ PublicLooks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PublicLooks.MvcApplication" Language="C#" %>
---
+++ PublicLooks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PublicLooks.Startup))]
+namespace PublicLooks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
